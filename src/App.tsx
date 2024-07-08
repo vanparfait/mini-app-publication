d2f7c0ce8d8a86c3bf1e20b4328f8a79e7ddf235
vanparfait <nguemechiaparfait@gmail.com>
2024-07-08 12:57:42 +0100

    Mise en place du routeur

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,19 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
 function App() {
-  return <>ttttt</>;
+  return (
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<h1>page d'accueil</h1>} />
+          <Route
+            path="/publication/:id"
+            element={<h1>page de publication</h1>}
+          />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
